615dff470ec09031b68b7f51cbc8bb757a3fb041
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-04-28 14:55:44 -0300

    Dto and Messaje Class

--- portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/AuthContoller.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/AuthContoller.java
@@ -13,9 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +38,8 @@ public class AuthContoller {
     RolService rolService;
     @Autowired
     JwtProvider jwtProvider;
-    
+  
+    @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         if (bindingResult.hasErrors()){
             return new ResponseEntity(new Mensaje("Campos mal colocados o email invalido"),HttpStatus.BAD_REQUEST);
@@ -50,5 +55,34 @@ public class AuthContoller {
         
         Set<Rol> roles = new HashSet<>();
         roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+        
+        if (nuevoUsuario.getRoles().contains("admin")){
+            roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+        }
+        
+        usuario.setRoles(roles);
+        usuarioService.save(usuario);
+        
+        return new ResponseEntity(new Mensaje("El usuario ha sido guardado"),HttpStatus.CREATED );
+            
+    }
+    
+    @PostMapping("/loggin")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity(new Mensaje("Los campos estan mal puestos"),HttpStatus.BAD_REQUEST);
+        }
+        
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDto JwtDto = new JwtDto(jwt, UserDetails.getUsername(), UserDetails.getAuthorities);
+        
+        return new ResponseEntity(jwtDto,HttpStatus.OK);
     }
 }
---
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/Mnesaje.java
@@ -0,0 +1,29 @@
+package com.portfolio.portfolio.Security.Controller;
+
+
+public class Mnesaje {
+    private String mensaje;
+    
+    //constructores
+
+    public Mnesaje() {
+    }
+
+    public Mnesaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //constructores
+    
+    //getters y setters
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //getters y setters
+}
