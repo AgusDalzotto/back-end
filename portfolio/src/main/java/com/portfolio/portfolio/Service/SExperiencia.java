228a1eb6cd7ae63eb8347d0fb14306d855e10ee2
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 17:54:22 -0300

    experience

--- portfolio/src/main/java/com/portfolio/portfolio/Service/SExperiencia.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Service/SExperiencia.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.portfolio.Service;
 
-/**
- *
- * @author Agus
- */
+import com.portfolio.portfolio.Entity.Experiencia;
+import com.portfolio.portfolio.Repository.RExperiencia;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class SExperiencia {
+    @Autowired
+    RExperiencia rExperiencia;
     
+    public List<Experiencia> list(){
+        return rExperiencia.findAll();
+    }
+    
+    public Optional<Experiencia> getOne(int id){
+        return rExperiencia.findById(id);
+    }
 }
