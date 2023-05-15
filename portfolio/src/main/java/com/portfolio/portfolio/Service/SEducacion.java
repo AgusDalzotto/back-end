2f63cef29d673f67f07e7d6dbed666375ffd352e
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 23:54:53 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Service/SEducacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Service/SEducacion.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.portfolio.Service;
 
-/**
- *
- * @author Agus
- */
+import com.portfolio.portfolio.Security.Entity.Educacion;
+import com.portfolio.portfolio.Security.Repository.REducacion;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class SEducacion {
+    REducacion rEducacion;
     
+    public List<Educacion> list(){
+        return rEducacion.findAll();
+    }
+    
+    public Optional<Educacion> getOne(int id){
+        return rEducacion.findById(id);
+    }
+    
+    public Optional<Educacion> getByNombreE(String nombreE){
+        return rEducacion.findByNombreE(nombreE);
+    }
+    
+      public void save(Educacion educacion){
+        rEducacion.save(educacion);
+    }
+    
+    public void delete(int id){
+        rEducacion.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rEducacion.existsById(id);
+    }
+    
+    public boolean existsByNombreE(String nombreE){
+        return rEducacion.existsByNombreE(nombreE);
+    }
 }
