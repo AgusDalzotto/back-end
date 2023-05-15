81594572ab7073027ad917797adc41d8d06d0458
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 18:48:32 -0300

    fixing cors

--- src/main/java/com/portfolio/portfolio/Controler/PersonaController.java
+++ src/main/java/com/portfolio/portfolio/Controler/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendarg-1de98.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
