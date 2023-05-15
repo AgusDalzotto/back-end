c0c4278255394f9bdafe82b18441fe88cca3bdce
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 23:46:53 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Security/Entity/Educacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Entity/Educacion.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.portfolio.Security.Entity;
 
-/**
- *
- * @author Agus
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Educacion {
-    
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreE;
+    private String descripcionE;
+
+    public Educacion() {
+    }
+
