c2978d360a6005dc096e9b25a5583af79999984f
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 00:02:47 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/CEducacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Controller/CEducacion.java
@@ -1,6 +1,8 @@
 
 package com.portfolio.portfolio.Security.Controller;
 
+import com.portfolio.portfolio.Service.SEducacion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/educacion")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
-    
+    @Autowired
+    SEducacion sEducacion;
 }
