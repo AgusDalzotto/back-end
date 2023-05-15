cce306cddae3b8a78cf09836de1cb7922ffbc852
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 09:10:16 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Controler/CEducacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Controler/CEducacion.java
@@ -1,10 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.portfolio.Controler;
 
+import com.portfolio.portfolio.Entity.Educacion;
+import com.portfolio.portfolio.Service.SEducacion;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/educacion")
 public class CEducacion {
+    @Autowired
+    SEducacion sEducacion;
     
+    @GetMapping("/lista")
+    public ResponseEntity<List<Educacion>> list(){
+        List<Educacion> list = sEducacion.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
 }
