86f8197e6ebfc364ca72aef553c749323d41e718
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 23:58:52 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Security/Dto/dtoEducacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Dto/dtoEducacion.java
@@ -8,4 +8,32 @@ public class dtoEducacion {
     private String nombreE;
     @NotBlank
     private String descripcionE;
+    
+    //constructores
+    public dtoEducacion() {
+    }
+
+    public dtoEducacion(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    //getters y setters
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
 }
