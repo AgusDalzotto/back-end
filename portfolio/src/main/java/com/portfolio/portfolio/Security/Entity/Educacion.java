a267e59e6b03f1313828511f0db123cc80c512f4
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 23:47:57 -0300

    edu

--- portfolio/src/main/java/com/portfolio/portfolio/Security/Entity/Educacion.java
+++ portfolio/src/main/java/com/portfolio/portfolio/Security/Entity/Educacion.java
@@ -16,4 +16,34 @@ public class Educacion {
 
     public Educacion() {
     }
+    
+ public Educacion(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+}
 
