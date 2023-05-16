fba328fa411dd3747b0375ba7858f0746cf184fd
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-16 10:31:51 -0300

    login funcional

--- src/main/java/com/portfolio/portfolio/Security/Controller/AuthContoller.java
+++ src/main/java/com/portfolio/portfolio/Security/Controller/AuthContoller.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "https://frontendarg-1de98.web.app")
+@CrossOrigin (origins = "http://localhost:4200")
 public class AuthContoller {
 
     @Autowired
