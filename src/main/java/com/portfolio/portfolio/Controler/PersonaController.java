4533b2e349dda255d26ce79f6b6776a0d9183cdd
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 22:41:34 -0300

    adding

--- src/main/java/com/portfolio/portfolio/Controler/PersonaController.java
+++ src/main/java/com/portfolio/portfolio/Controler/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("personas")
+@RequestMapping("/personas")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
